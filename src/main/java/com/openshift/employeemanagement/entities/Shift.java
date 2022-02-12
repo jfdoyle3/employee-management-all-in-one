e6a09c77425c8d5ca56d17511a7e8ee1f6b5db88
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2022-02-11 23:46:39 -0500

    added punch in and punch out

--- src/main/java/com/openshift/employeemanagement/entities/Shift.java
+++ src/main/java/com/openshift/employeemanagement/entities/Shift.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -38,6 +39,51 @@ public class Shift {
     public Shift() {
     }
 
+    public Shift(Date punchIn, Date punchOut, Double hours) {
+        this.punchIn = punchIn;
+        this.punchOut = punchOut;
+        this.hours = hours;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getPunchIn() {
+        return punchIn;
+    }
+
+    public void setPunchIn(Date punchIn) {
+        this.punchIn = punchIn;
+    }
+
+    public Date getPunchOut() {
+        return punchOut;
+    }
+
+    public void setPunchOut(Date punchOut) {
+        this.punchOut = punchOut;
+    }
+
+    public Double getHours() {
+        return hours;
+    }
+
+    public void setHours(Double hours) {
+        this.hours = hours;
+    }
+
+    public Set<Employee> getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Set<Employee> employee) {
+        this.employee = employee;
+    }
 }
 
 
