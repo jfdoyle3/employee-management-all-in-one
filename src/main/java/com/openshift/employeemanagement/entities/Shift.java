3bb96a49d749b04bbae0947f779d994d27f2be06
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2022-02-11 23:44:20 -0500

    added punch in and punch out

--- src/main/java/com/openshift/employeemanagement/entities/Shift.java
+++ src/main/java/com/openshift/employeemanagement/entities/Shift.java
@@ -21,14 +21,13 @@ public class Shift {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String shiftType;
-    private String shiftStart;
-    private String shiftEnd;
+    private Date punchIn;
+    private Date punchOut;
     private Double hours;
 
     @ManyToMany
     @JoinTable(
-            name="employee_shift",
+            name = "employee_shift",
             joinColumns = @JoinColumn(name = "shift_id"),
             inverseJoinColumns = @JoinColumn(name = "employee_id")
     )
@@ -37,70 +36,8 @@ public class Shift {
 
 
     public Shift() {
-
     }
 
-    public Shift(String shiftType, String shiftStart, String shiftEnd, Double hours) {
-        this.shiftType = shiftType;
-        this.shiftStart = shiftStart;
-        this.shiftEnd = shiftEnd;
-        this.hours = hours;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getShiftType() {
-        return shiftType;
-    }
-
-    public  void setShiftType(String shiftType) {
-        this.shiftType = shiftType;
-    }
-
-    public String getShiftStart() {
-        return shiftStart;
-    }
-
-    public void setShiftStart(String shiftStart) {
-        this.shiftStart = shiftStart;
-    }
-
-    public String getShiftEnd() {
-        return shiftEnd;
-    }
-
-    public  void setShiftEnd(String shiftEnd) {
-        this.shiftEnd = shiftEnd;
-    }
-
-//    public Double calculateHours(String shiftStart, String shiftEnd) {
-//        return Double.parseDouble(shiftEnd) - Double.parseDouble(shiftStart);
-//    }
-
-//    public HashMap<Employee, Shift> assignShifts() {
-//
-//    }
-
-
-    public Set<Employee> getEmployee() {
-        return employee;
-    }
-
-    public void setEmployee(Set<Employee> employee) {
-        this.employee= employee;
-    }
-
-    public Double getHours() {
-        return hours;
-    }
-
-    public void setHours(Double hours) {
-        this.hours = hours;
-    }
 }
+
+
